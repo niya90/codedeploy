ed2f70b18b5a935202ec332820f2d7e3a3a863da
niya90 <niyati.upadhyaya@gmail.com>
2020-02-18 08:15:14 +0530

    test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
+# SPDX-License-Identifier: MIT-01
 
 from web import myapp
 import unittest
